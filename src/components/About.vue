f699f7f6247064620afc0c81004306ca53a456e8
sigvaldisigurdar <97695773+sigvaldisigurdar@users.noreply.github.com>
2022-05-25 22:30:57 +0000

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="content">
     <h1 class="title is-1 has-text-centered">Um Stuðningsbankann</h1>
-    <p>Stuðningsbankinn er heimasíða á vegum Sambands íslenskra framhaldsskólanema (SÍF). Síðunni er ætlað að veita nemendum með sértæka námsörðugleika og/eða fatlanir gagnlegar upplýsingar um þjónustu og aðgengi í framhaldsskólum landssins. Einnig má finna á síðunni ýmsar upplýsingar um almenn réttindi þessara nemenda auk annars fróðleiks eins og hlekki inn á síður sem geta hjálpað við námið.</p>
-    <p>Stuðningsbankinn var unninn af hóp nemenda í MPM námi við Háskólann í Reykjavík. Hópurinn sá um upplýsingaöflun, hönnun og uppsetningu síðunnar. Einnig útbjó hópurinn og setti upp spurningalista sem sendur var á skólastjórnendur og námsráðgjafa til að fá upplýsingar um aðgengi og þjónustu í öllum framhaldsskólum landsins. </p>
-    <p>Hópurinn afhendi SÍF beta útgáfu af vefsíðunni þar sem tveir skólar höfðu svarað spurningalistanum. SÍF sá um lokaútfærslu síðunnar og sá um að fá svör frá öllum framhaldsskólum landsins til að birta á síðunni.</p>
+    <p>Stuðningsbankinn er heimasíða á vegum Sambands íslenskra framhaldsskólanema (SÍF). Stuðningsbankinn gefur fólki með námsþarfir loksins kleift að vita hvernig úrræðum, aðgengi og þjónustu er háttað í öllum framhaldsskólum á landinu á einum stað! Einnig má finna á síðunni ýmsar gagnlegar upplýsingar sem geta aðstoðað við námið, stofnanir og félagasamtök sem gott er að vita af og lög og reglur sem tengjast sérúrræðum á einn eða annan hátt.
+Verkefnið er einstakt því það er unnið af nemendum fyrir nemendur. Öll vefsíðan er uppsett eftir ítarlega þarfagreiningu nemenda, tugi rýnihópaviðtala, funda ásamt notendaprófunum.</p>
+    <p>Sigvaldi Sigurðarson er verkefnastjóri Stuðningsbankans og Bergur Hallgrímsson vefstjóri.<p>
+    <p>Fyrsti hluti verkefnisins var unnin af hópi nemenda í MPM námi við Háskólann í Reykjavík. Hópurinn sá um fyrsta hluta upplýsingaöflunar, hönnun og uppsetningu síðunnar. Einnig útbjó hópurinn og setti upp gátlista sem sendur var á skólastjórnendur og námsráðgjafa til að fá upplýsingar um aðgengi og þjónustu í öllum framhaldsskólum landsins. </p>
+    <p>Hópurinn afhendi SÍF beta útgáfu af vefsíðunni þar sem tveir skólar höfðu svarað gátlistanum. SÍF sá þá um að bæta gátlista sem sendur var svo á alla skóla á landinu en leitað var til ráða ýmissa á aðila frá Þroskahjálp, Öryrkjabandalagsins, Blindrafélagið og Réttindagæslu fatlaðs fólks svo eitthvað sé nefnd. Vel var hugað að aðgengi á síðunni en Auðlesið mál hjá Þroskahjálp gáfu ítarlega endurgjöf á síðunni og gerðu texta aðgengilegri og keypt var háþróuð vefþula í gegnum Blindrafélagið sem einnig getur framkallað lesblinduletur. Frekari þróun átti sér stað á síðunni og stjórnkerfinu áður en lokaútgáfa var gefin út. </p>
+    
+    
   </div>
 </template>
 
